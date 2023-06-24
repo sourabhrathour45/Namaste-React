87730fece83e9daebf4752dd9cb78b5d1316c147
Sourabh <rathoursourabh5@gmail.com>
2023-06-24 13:29:21 +0530

    app initiated

--- app.js
+++ app.js
@@ -1,13 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
+const Header = () => {
+  return (
+    <nav id="header">
+      <div id="image-container">
+        <img
+          id="logo"
+          alt="logo"
+          src="https://drive.google.com/uc?export=download&id=1PrOLgDbWl_W2l9ouvkz1RLvZiOsls2HJ"
+        ></img>
+      </div>
+      <div>
+        <ul id="nav-links">
+          <li>Home</li>
+          <li>Restaurants</li>
+          <li>Your Orders</li>
+          <li>About Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
-const Heading = () => (
-    <div id="container">
-        <h1 className="heading">A Functional React Component</h1>
-    </div>
-)
+const Search = () => {
+  return <div></div>;
+};
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const AppLayout = () => {
+  return (
+    <>
+      <Header />
+      <Search />
+    </>
+  );
+};
 
-root.render(<Heading />)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<AppLayout />);
