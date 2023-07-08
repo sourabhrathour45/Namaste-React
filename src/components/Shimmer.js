f77b6abff42e64b1964a63a1a47659ac7bf6a1c2
Sourabh <rathoursourabh5@gmail.com>
2023-07-09 02:03:47 +0530

    Shimmer UI updated

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,105 +1,83 @@
 const Shimmer = () => {
   return (
-    <div className="container-div">
-      <div className="shimmer-container">
-        <div className="shimmer-card">
-          <div className="shimmer-card-image"></div>
-          <div className="shimmer-title"></div>
-          <div className="shimmer-card-description"></div>
-          <div className="shimmer-card-description-secondary"></div>
-        </div>
-        <div className="shimmer-card">
-          <div className="shimmer-card-image"></div>
-          <div className="shimmer-title"></div>
-          <div className="shimmer-card-description"></div>
-          <div className="shimmer-card-description-secondary"></div>
-        </div>
-        <div className="shimmer-card">
-          <div className="shimmer-card-image"></div>
-          <div className="shimmer-title"></div>
-          <div className="shimmer-card-description"></div>
-          <div className="shimmer-card-description-secondary"></div>
-        </div>
-        <div className="shimmer-card">
-          <div className="shimmer-card-image"></div>
-          <div className="shimmer-title"></div>
-          <div className="shimmer-card-description"></div>
-          <div className="shimmer-card-description-secondary"></div>
-        </div>
-        <div className="shimmer-card">
-          <div className="shimmer-card-image"></div>
-          <div className="shimmer-title"></div>
-          <div className="shimmer-card-description"></div>
-          <div className="shimmer-card-description-secondary"></div>
-        </div>
-        <div className="shimmer-card">
-          <div className="shimmer-card-image"></div>
-          <div className="shimmer-title"></div>
-          <div className="shimmer-card-description"></div>
-          <div className="shimmer-card-description-secondary"></div>
-        </div>
-        <div className="shimmer-card">
-          <div className="shimmer-card-image"></div>
-          <div className="shimmer-title"></div>
-          <div className="shimmer-card-description"></div>
-          <div className="shimmer-card-description-secondary"></div>
-        </div>
-        <div className="shimmer-card">
-          <div className="shimmer-card-image"></div>
-          <div className="shimmer-title"></div>
-          <div className="shimmer-card-description"></div>
-          <div className="shimmer-card-description-secondary"></div>
-        </div>
-        <div className="shimmer-card">
-          <div className="shimmer-card-image"></div>
-          <div className="shimmer-title"></div>
-          <div className="shimmer-card-description"></div>
-          <div className="shimmer-card-description-secondary"></div>
-        </div>
-        <div className="shimmer-card">
-          <div className="shimmer-card-image"></div>
-          <div className="shimmer-title"></div>
-          <div className="shimmer-card-description"></div>
-          <div className="shimmer-card-description-secondary"></div>
-        </div>
-        <div className="shimmer-card">
-          <div className="shimmer-card-image"></div>
-          <div className="shimmer-title"></div>
-          <div className="shimmer-card-description"></div>
-          <div className="shimmer-card-description-secondary"></div>
-        </div>
-        <div className="shimmer-card">
-          <div className="shimmer-card-image"></div>
-          <div className="shimmer-title"></div>
-          <div className="shimmer-card-description"></div>
-          <div className="shimmer-card-description-secondary"></div>
-        </div>
-        <div className="shimmer-card">
-          <div className="shimmer-card-image"></div>
-          <div className="shimmer-title"></div>
-          <div className="shimmer-card-description"></div>
-          <div className="shimmer-card-description-secondary"></div>
-        </div>
-        <div className="shimmer-card">
-          <div className="shimmer-card-image"></div>
-          <div className="shimmer-title"></div>
-          <div className="shimmer-card-description"></div>
-          <div className="shimmer-card-description-secondary"></div>
-        </div>
-        <div className="shimmer-card">
-          <div className="shimmer-card-image"></div>
-          <div className="shimmer-title"></div>
-          <div className="shimmer-card-description"></div>
-          <div className="shimmer-card-description-secondary"></div>
-        </div>
-        <div className="shimmer-card">
-          <div className="shimmer-card-image"></div>
-          <div className="shimmer-title"></div>
-          <div className="shimmer-card-description"></div>
-          <div className="shimmer-card-description-secondary"></div>
+    <>
+      <div className="flex justify-center mt-4 lg:mt-12 w-screen">
+        <div className="flex flex-wrap md:w-2/3">
+          <div className="lg:w-56 lg:h-80 w-11/12 h-72 p-4 mx-8 my-8 rounded-lg bg-[#FFF6E7] drop-shadow-md hover:drop-shadow-xl">
+            <div className="bg-[#FEEAC8] lg:w-50 h-36 lg:m-2 lg:h-28 "></div>
+            <div className="lg:w-44 lg:h-5 w-10/12 mt-6 h-5 bg-[#FEEAC8] lg:m-2 lg:mt-6 rounded-sm"></div>
+            <div className="lg:w-36 lg:h-3  bg-[#FEEAC8] lg:m-2 lg:mt-6 rounded-sm"></div>
+            <div className="lg:w-44 lg:h-4 w-72 mt-6 h-4 bg-[#FEEAC8] lg:m-2 lg:mt-6 rounded-sm"></div>
+          </div>
+          <div className="lg:w-56 lg:h-80 w-11/12 h-72 p-4 mx-8 my-8 rounded-lg bg-[#FFF6E7] drop-shadow-md hover:drop-shadow-xl">
+            <div className="bg-[#FEEAC8] lg:w-50 h-36 lg:m-2 lg:h-28 "></div>
+            <div className="lg:w-44 lg:h-5 w-10/12 mt-6 h-5 bg-[#FEEAC8] lg:m-2 lg:mt-6 rounded-sm"></div>
+            <div className="lg:w-36 lg:h-3  bg-[#FEEAC8] lg:m-2 lg:mt-6 rounded-sm"></div>
+            <div className="lg:w-44 lg:h-4 w-72 mt-6 h-4 bg-[#FEEAC8] lg:m-2 lg:mt-6 rounded-sm"></div>
+          </div>
+          <div className="lg:w-56 lg:h-80 w-11/12 h-72 p-4 mx-8 my-8 rounded-lg bg-[#FFF6E7] drop-shadow-md hover:drop-shadow-xl">
+            <div className="bg-[#FEEAC8] lg:w-50 h-36 lg:m-2 lg:h-28 "></div>
+            <div className="lg:w-44 lg:h-5 w-10/12 mt-6 h-5 bg-[#FEEAC8] lg:m-2 lg:mt-6 rounded-sm"></div>
+            <div className="lg:w-36 lg:h-3  bg-[#FEEAC8] lg:m-2 lg:mt-6 rounded-sm"></div>
+            <div className="lg:w-44 lg:h-4 w-72 mt-6 h-4 bg-[#FEEAC8] lg:m-2 lg:mt-6 rounded-sm"></div>
+          </div>
+          <div className="lg:w-56 lg:h-80 w-11/12 h-72 p-4 mx-8 my-8 rounded-lg bg-[#FFF6E7] drop-shadow-md hover:drop-shadow-xl">
+            <div className="bg-[#FEEAC8] lg:w-50 h-36 lg:m-2 lg:h-28 "></div>
+            <div className="lg:w-44 lg:h-5 w-10/12 mt-6 h-5 bg-[#FEEAC8] lg:m-2 lg:mt-6 rounded-sm"></div>
+            <div className="lg:w-36 lg:h-3  bg-[#FEEAC8] lg:m-2 lg:mt-6 rounded-sm"></div>
+            <div className="lg:w-44 lg:h-4 w-72 mt-6 h-4 bg-[#FEEAC8] lg:m-2 lg:mt-6 rounded-sm"></div>
+          </div>
+          <div className="lg:w-56 lg:h-80 w-11/12 h-72 p-4 mx-8 my-8 rounded-lg bg-[#FFF6E7] drop-shadow-md hover:drop-shadow-xl">
+            <div className="bg-[#FEEAC8] lg:w-50 h-36 lg:m-2 lg:h-28 "></div>
+            <div className="lg:w-44 lg:h-5 w-10/12 mt-6 h-5 bg-[#FEEAC8] lg:m-2 lg:mt-6 rounded-sm"></div>
+            <div className="lg:w-36 lg:h-3  bg-[#FEEAC8] lg:m-2 lg:mt-6 rounded-sm"></div>
+            <div className="lg:w-44 lg:h-4 w-72 mt-6 h-4 bg-[#FEEAC8] lg:m-2 lg:mt-6 rounded-sm"></div>
+          </div>
+          <div className="lg:w-56 lg:h-80 w-11/12 h-72 p-4 mx-8 my-8 rounded-lg bg-[#FFF6E7] drop-shadow-md hover:drop-shadow-xl">
+            <div className="bg-[#FEEAC8] lg:w-50 h-36 lg:m-2 lg:h-28 "></div>
+            <div className="lg:w-44 lg:h-5 w-10/12 mt-6 h-5 bg-[#FEEAC8] lg:m-2 lg:mt-6 rounded-sm"></div>
+            <div className="lg:w-36 lg:h-3  bg-[#FEEAC8] lg:m-2 lg:mt-6 rounded-sm"></div>
+            <div className="lg:w-44 lg:h-4 w-72 mt-6 h-4 bg-[#FEEAC8] lg:m-2 lg:mt-6 rounded-sm"></div>
+          </div>
+          <div className="lg:w-56 lg:h-80 w-11/12 h-72 p-4 mx-8 my-8 rounded-lg bg-[#FFF6E7] drop-shadow-md hover:drop-shadow-xl">
+            <div className="bg-[#FEEAC8] lg:w-50 h-36 lg:m-2 lg:h-28 "></div>
+            <div className="lg:w-44 lg:h-5 w-10/12 mt-6 h-5 bg-[#FEEAC8] lg:m-2 lg:mt-6 rounded-sm"></div>
+            <div className="lg:w-36 lg:h-3  bg-[#FEEAC8] lg:m-2 lg:mt-6 rounded-sm"></div>
+            <div className="lg:w-44 lg:h-4 w-72 mt-6 h-4 bg-[#FEEAC8] lg:m-2 lg:mt-6 rounded-sm"></div>
+          </div>
+          <div className="lg:w-56 lg:h-80 w-11/12 h-72 p-4 mx-8 my-8 rounded-lg bg-[#FFF6E7] drop-shadow-md hover:drop-shadow-xl">
+            <div className="bg-[#FEEAC8] lg:w-50 h-36 lg:m-2 lg:h-28 "></div>
+            <div className="lg:w-44 lg:h-5 w-10/12 mt-6 h-5 bg-[#FEEAC8] lg:m-2 lg:mt-6 rounded-sm"></div>
+            <div className="lg:w-36 lg:h-3  bg-[#FEEAC8] lg:m-2 lg:mt-6 rounded-sm"></div>
+            <div className="lg:w-44 lg:h-4 w-72 mt-6 h-4 bg-[#FEEAC8] lg:m-2 lg:mt-6 rounded-sm"></div>
+          </div>
+          <div className="lg:w-56 lg:h-80 w-11/12 h-72 p-4 mx-8 my-8 rounded-lg bg-[#FFF6E7] drop-shadow-md hover:drop-shadow-xl">
+            <div className="bg-[#FEEAC8] lg:w-50 h-36 lg:m-2 lg:h-28 "></div>
+            <div className="lg:w-44 lg:h-5 w-10/12 mt-6 h-5 bg-[#FEEAC8] lg:m-2 lg:mt-6 rounded-sm"></div>
+            <div className="lg:w-36 lg:h-3  bg-[#FEEAC8] lg:m-2 lg:mt-6 rounded-sm"></div>
+            <div className="lg:w-44 lg:h-4 w-72 mt-6 h-4 bg-[#FEEAC8] lg:m-2 lg:mt-6 rounded-sm"></div>
+          </div>
+          <div className="lg:w-56 lg:h-80 w-11/12 h-72 p-4 mx-8 my-8 rounded-lg bg-[#FFF6E7] drop-shadow-md hover:drop-shadow-xl">
+            <div className="bg-[#FEEAC8] lg:w-50 h-36 lg:m-2 lg:h-28 "></div>
+            <div className="lg:w-44 lg:h-5 w-10/12 mt-6 h-5 bg-[#FEEAC8] lg:m-2 lg:mt-6 rounded-sm"></div>
+            <div className="lg:w-36 lg:h-3  bg-[#FEEAC8] lg:m-2 lg:mt-6 rounded-sm"></div>
+            <div className="lg:w-44 lg:h-4 w-72 mt-6 h-4 bg-[#FEEAC8] lg:m-2 lg:mt-6 rounded-sm"></div>
+          </div>
+          <div className="lg:w-56 lg:h-80 w-11/12 h-72 p-4 mx-8 my-8 rounded-lg bg-[#FFF6E7] drop-shadow-md hover:drop-shadow-xl">
+            <div className="bg-[#FEEAC8] lg:w-50 h-36 lg:m-2 lg:h-28 "></div>
+            <div className="lg:w-44 lg:h-5 w-10/12 mt-6 h-5 bg-[#FEEAC8] lg:m-2 lg:mt-6 rounded-sm"></div>
+            <div className="lg:w-36 lg:h-3  bg-[#FEEAC8] lg:m-2 lg:mt-6 rounded-sm"></div>
+            <div className="lg:w-44 lg:h-4 w-72 mt-6 h-4 bg-[#FEEAC8] lg:m-2 lg:mt-6 rounded-sm"></div>
+          </div>
+          <div className="lg:w-56 lg:h-80 w-11/12 h-72 p-4 mx-8 my-8 rounded-lg bg-[#FFF6E7] drop-shadow-md hover:drop-shadow-xl">
+            <div className="bg-[#FEEAC8] lg:w-50 h-36 lg:m-2 lg:h-28 "></div>
+            <div className="lg:w-44 lg:h-5 w-10/12 mt-6 h-5 bg-[#FEEAC8] lg:m-2 lg:mt-6 rounded-sm"></div>
+            <div className="lg:w-36 lg:h-3  bg-[#FEEAC8] lg:m-2 lg:mt-6 rounded-sm"></div>
+            <div className="lg:w-44 lg:h-4 w-72 mt-6 h-4 bg-[#FEEAC8] lg:m-2 lg:mt-6 rounded-sm"></div>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
