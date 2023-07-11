f3526d478f03902331695bbfe55ffc6fd51e939c
Sourabh <rathoursourabh5@gmail.com>
2023-07-11 23:12:22 +0530

    experiment

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,60 @@
-const Contact = ()=>{
-    return(
-        <div>
-            <h1>Contact Us Page</h1>
-        </div>
-    )
-}
+import { useState } from "react";
+
+const Section = ({ title, description }) => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  return (
+    <div className="m-4 p-4 border border-orange-800 rounded-lg">
+      <h1 className="text-3xl font-bold text-orange-950">{title}</h1>
+      {isVisible ? (
+        <button
+          className="py-2 px-4 bg-orange-700 text-white rounded-lg text-sm hover:bg-orange-800 mt-4 mb-4"
+          onClick={() => setIsVisible(false)}
+        >
+          Collapse
+        </button>
+      ) : (
+        <button
+          className="py-2 px-4 bg-orange-700 text-white rounded-lg text-sm hover:bg-orange-800 mt-4 mb-4"
+          onClick={() => setIsVisible(true)}
+        >
+          Expand
+        </button>
+      )}
+
+      {isVisible ? <p>{description}</p> : null}
+    </div>
+  );
+};
+
+const Contact = () => {
+  return (
+    <>
+        <Section
+      title={"Who are we?"}
+      description={
+        "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains."
+      }
+    />
+
+<Section
+      title={"Our Vision"}
+      description={
+        "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains."
+      }
+    />
+
+<Section
+      title={"From the founder's note"}
+      description={
+        "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains."
+      }
+    />
+    </>
+
+
+    
+  );
+};
 
 export default Contact;
--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -11,17 +11,18 @@ const RestaurantMenu = () => {
       <>
         <div className="flex justify-center mt-24">
           <h1 className="text-2xl text--slate-600">
-          Please Wait... Fetching the Menu
+            Please Wait... Fetching the Menu
           </h1>
-          
-          </div>
+        </div>
       </>
     );
   }
 
   const { name, cuisines } = resInfo?.cards[0]?.card?.card?.info;
   const { itemCards } =
-    resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card || resInfo?.cards[3]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card ;
+    resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card
+      ?.card ||
+    resInfo?.cards[3]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card;
 
   return (
     <div className="flex justify-center mt-24 w-full">
@@ -30,15 +31,28 @@ const RestaurantMenu = () => {
         <br></br>
         <p className="text-slate-700"> {cuisines.join(", ")}</p>
         <br></br>
-        <h3 className="text-xl font-bold text-orange-900 bg-orange-200 drop-shadow-lg p-4 rounded-lg text-center"> 📍 Menu </h3>
+        <h3 className="text-xl font-bold text-orange-900 bg-orange-200 drop-shadow-lg p-4 rounded-lg text-center">
+          {" "}
+          📍 Menu{" "}
+        </h3>
         <br></br>
         <br></br>
         <ul>
           {itemCards?.map((item) => (
-            <li className="text-lg py-8 px-4 font-Inter bg-[#FFF6E7] mb-4 drop-shadow-lg rounded-lg" key={item?.card?.info?.id}>
+            <li
+              className=" flex justify-between  text-lg py-8 px-4 font-Inter bg-[#FFF6E7] mb-4 drop-shadow-lg rounded-lg"
+              key={item?.card?.info?.id}
+            >
               {item?.card?.info?.name} : ₹{" "}
               {item?.card?.info?.price / 100 ||
                 item?.card?.info?.defaultPrice / 100}
+              {item?.card?.info?.imageId ? (
+                <img
+                  className="overflow-hidden lg:w-36 rounded-sm"
+                  alt="item-img"
+                  src={`https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/${item?.card?.info?.imageId}`}
+                />
+              ) : null}
             </li>
           ))}
         </ul>
