00e7099279511bfa600aebb2b8f1e583a894f8a4
Sourabh <rathoursourabh5@gmail.com>
2023-08-03 23:52:25 +0530

    Configured redux store

--- src/ultils/appStore.js
+++ src/ultils/appStore.js
@@ -1,6 +1,7 @@
 import {configureStore} from '@reduxjs/toolkit'
 import cartReducer from './cartSlice'
 
+// configured the store here using redux
 const appStore = configureStore({
     reducer: {
         cart : cartReducer,
